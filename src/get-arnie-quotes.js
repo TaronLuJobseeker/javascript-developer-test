5fa97a41a5c06e915a975c2361d342aaaae30018
TaronLuJobseeker <taron.lu.jobseeker@gmail.com>
2021-11-10 11:10:04 +1100

    completed tech test

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,22 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const result = await Promise.all(
+    urls.map(async (url) => {
+      const { body, status } = await httpGet(url);
+      const { message } = JSON.parse(body);
+
+      if (status === 200) {
+        return { "Arnie Quote": message };
+      }
+
+      if (status === 500) {
+        return { FAILURE: message };
+      }
+    })
+  );
+
+  return result;
 };
 
 module.exports = {
